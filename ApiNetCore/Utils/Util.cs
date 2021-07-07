863560aaba48114b76614024438dfe23b13eee90
FPolich <75345608+FPolich@users.noreply.github.com>
2021-07-07 08:40:59 -0300

    Add files via upload
    api net core

---
+++ ApiNetCore/Utils/Util.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiNetCore.Utils
+{
+    public static class Util
+    {
+        public static MySqlConnection getConnection()
+        {
+            MySqlConnection miConexion = new MySqlConnection();
+            miConexion.ConnectionString = "Server=127.0.0.1;Uid=root;Pwd=1234;Database=SpotifyLocalBDD";
+            miConexion.Open();
+            return miConexion;
+        }
+    }
+}
